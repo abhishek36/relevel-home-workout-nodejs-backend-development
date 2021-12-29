9d8114b3d6a85ff2cf876c1727dface8de2c1a2a
abhishek36 <akamble9913@gmail.com>
2021-12-29 10:45:05 +0530

    first commit

---
+++ src/controllers/exercise.controller.js
@@ -0,0 +1,30 @@
+//import Exercise model here
+
+const addExercise = async (req, res) => {
+    //addExercise api logic here
+};
+
+const getExerciseOnCategoryAndLevel = async (req, res) => {
+    //getExerciseOnCategoryAndLevel api logic here
+};
+
+const getExerciseById = async (req, res) => {
+    //getExerciseById api logic here
+};
+
+const updateExerciseById = async (req, res) => {
+    //updateExerciseById api logic here
+};
+
+const deleteExerciseById = async (req, res) => {
+    //deleteExerciseById api logic here
+};
+
+const ExerciseController = {
+    addExercise,
+    getExerciseOnCategoryAndLevel,
+    getExerciseById,
+    updateExerciseById,
+    deleteExerciseById
+};
+module.exports = ExerciseController;
